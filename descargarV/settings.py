eb435c727af97ca28d87c9d7eb8cd7a2f3d1c39f
AnJoGar <andjor2020@gmail.com>
2025-11-20 23:59:49 -0500

    s

--- descargarV/settings.py
+++ descargarV/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 
-ALLOWED_HOSTS = ['*']
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
